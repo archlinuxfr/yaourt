06a7a337dbbac940378fc1d71ca4e3114b872b4f
tuxce <tuxce.net@gmail.com>
2010-06-18 00:18:34 +0200

    pacman 3.4 drops .lastupdate for /var/lib/pacman/sync/$repo/

--- src/lib/abs.sh
+++ src/lib/abs.sh
@@ -23,7 +23,7 @@ abs_get_pkgbuild ()
 	# TODO: store abs archive somewhere else.
 	local abs_tar="$YAOURTTMPDIR/$repo.abs.tar.gz"
 	local abs_url 
-	local repo_date=$(stat -c "%Z" "$PACMANDB/sync/$repo/.lastupdate")
+	local repo_date=$(stat -c "%Z" "$PACMANDB/$repo.db.tar.gz")
 	local abs_repo_date=$(stat -c "%Z" "$abs_tar" 2> /dev/null)
 	if (( $? )) || (( abs_repo_date < repo_date )); then
 		abs_url=$(pkgquery -1Sif "%u" "$repo/$pkg")
