1e14965360b602c737a91ced4141187aa08c235e
tuxce <tuxce.net@gmail.com>
2012-09-19 15:52:24 +0200

    Display "0M" instead of "null" for --stats

--- src/lib/alpm_stats.sh
+++ src/lib/alpm_stats.sh
@@ -101,7 +101,7 @@ show_disk_usage() {
 	cachedir=(`pacman_parse --debug 2>/dev/null | grep "^debug: option 'cachedir'" |awk '{print $5}'`)
 	# space used by download packages or sources in cache
 	echo -e "$CGREEN$(gettext 'Space used by pkg downloaded in cache (cachedir):') $CYELLOW $(du -sh "${P[cachedir]}" 2>/dev/null|awk '{print $1}')"
-	[[ $SRCDEST ]] && srcdestsize=`du -sh "$SRCDEST" 2>/dev/null|awk '{print $1}'` || srcdestsize=null
+	[[ $SRCDEST ]] && srcdestsize=`du -sh "$SRCDEST" 2>/dev/null|awk '{print $1}'` || srcdestsize="0M"
 	echo -e "${CGREEN}$(gettext 'Space used by src downloaded in cache:') $CYELLOW $srcdestsize$C0"
 }
 
